2c515882760906f8d60b205e467484c3df54009e
notDuyLam <22120181@student.hcmus.edu.vn>
2025-06-13 21:15:36 +0700

    implement basic of search score page

--- src/pages/Score.tsx
+++ src/pages/Score.tsx
@@ -1,7 +1,7 @@
 export default function Score() {
   return (
-    <div className="p-6 m-4 bg-white border border-gray-300 shadow-lg  rounded-lg">
-      <div className="registration-number p-2">
+    <>
+      <div className="registration-number p-8 m-4 bg-white border border-gray-300 shadow-lg  rounded-lg">
         <div className="text-lg font-bold py-2">User Registration</div>
         <label htmlFor="registration-number">Registration Number:</label>
         <br></br>
@@ -15,20 +15,10 @@ export default function Score() {
           Submit
         </button>
       </div>
-      <div className="detail-score p-2">
-        <div className="text-lg font-bold py-2">User Registration</div>
-        <label htmlFor="registration-number">Registration Number:</label>
-        <br></br>
-        <input
-          className="p-2 my-1 w-1/4 border-1 rounded"
-          id="registration-number"
-          type="text"
-          placeholder="Enter registration number"
-        />
-        <button className="bg-black text-white hover:cursor-pointer py-2 px-4 mx-2 rounded">
-          Submit
-        </button>
+      <div className="p-8 m-4 bg-white border border-gray-300 shadow-lg  rounded-lg">
+        <div className="text-lg font-bold py-2">Detailed Score</div>
+        <div className="">Detailed view of search score here</div>
       </div>
-    </div>
+    </>
   );
 }
